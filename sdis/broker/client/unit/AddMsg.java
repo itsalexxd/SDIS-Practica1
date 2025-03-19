a102e57effc28170e50636dc093ad46222742ddb
Alex <alejandro.garcia.lavandera23@estudiantes.uva.es>
2025-03-19 10:40:05 +0100

    Auth.java creado (posiblemente mal, pero hay algo)

--- sdis/broker/client/unit/AddMsg.java
+++ sdis/broker/client/unit/AddMsg.java
@@ -3,6 +3,5 @@ package sdis.broker.client.unit;
 import sdis.broker.common.*;
 
 public class AddMsg {
-    final private int PUERTO = 2000;
 
 }
--- sdis/broker/client/unit/Auth.java
+++ sdis/broker/client/unit/Auth.java
@@ -1,7 +1,36 @@
 package sdis.broker.client.unit;
 
+import java.io.*;
+import java.net.*;
+
 import sdis.broker.common.*;
 
 public class Auth {
+    private static final String SERVIDOR = "localhost";
+    final private int PUERTO = 2000;
+
+    public static boolean autenticar(String usuario, String clave){
+        try(Socket socket = new Socket(SERVIDOR, PUERTO));
+        BufferedReader inClinete = new BufferedReader(new InputStreamReader(Socket.getInputStream()));
+        PrintWriter outCliente = new PrintWriter(socket.getOutputStream(), true) {
+
+            // XAUTH: Usuario:
+                System.out.println(inCliente.readLine());
+                outCliente.println(usuario);
+
+            // XAUTH: Password:
+                System.out.println(inCliente.readLine());
+            outCliente(password);
+
+            String respuesta = inCliente.readLine();
+                System.out.println(respuesta);
+
+                return respuesta.contains("exitosa");
+
+        }catch(IOException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
 
 }
